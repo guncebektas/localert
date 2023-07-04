08f493ce59501aca39ede3ea22daf116ccc6b256
gunce <guncebektas@gmail.com>
2023-07-04 14:49:39 +0300

    add: webpack

---
+++ dist/bundle.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./index.js":
+/*!******************!*\
+  !*** ./index.js ***!
+  \******************/
+/***/ (() => {
+
+eval("function createLocalart() {\n  if (location.hostname === 'localhost') {\n    const elemDiv = document.createElement('div');\n    elemDiv.style.cssText = 'position:fixed;top:0;width:100%;height:5px;background:red;z-index:999999999';\n    document.body.appendChild(elemDiv);\n  }\n}\n\ndocument.addEventListener('DOMContentLoaded', createLocalart);\n\n//# sourceURL=webpack://@guncebektas/localert/./index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./index.js"]();
+/******/ 	
+/******/ })()
+;
--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-document.addEventListener("DOMContentLoaded", () => {
+function createLocalart() {
   if (location.hostname === 'localhost') {
     const elemDiv = document.createElement('div');
     elemDiv.style.cssText = 'position:fixed;top:0;width:100%;height:5px;background:red;z-index:999999999';
     document.body.appendChild(elemDiv);
   }
-});
+}
+
+document.addEventListener('DOMContentLoaded', createLocalart);
