3f8d8265cb7719c267c0e6f5191cf0d51e863227
gunce <guncebektas@gmail.com>
2023-07-04 11:16:20 +0300

    add: init

---
+++ README.md
@@ -0,0 +1,2 @@
+# LOCALERT
+This package creates a red line at the top of the window when host name is "localhost".
---
+++ index.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+  if (location.hostname === 'localhost') {
+    const elemDiv = document.createElement('div');
+    elemDiv.style.cssText = 'position:fixed;top:0;width:100%;height:5px;background:red;z-index:999999999';
+    document.body.appendChild(elemDiv);
+  }
+});
